df9f981335bafa960ed330e43c23988496a7d588
Konrad Zebro <konrad.zebro@gmail.com>
2025-06-05 18:00:34 +0200

    Added Client and Vehicle classes

---
+++ CarWorkshopManager/Models/Client.cs
@@ -0,0 +1,24 @@
+﻿namespace CarWorkshopManager.Models;
+
+public class Client(String name, String surname, String email)
+{
+    public int Id { get; set; }
+
+    public String Name { get; set; } = name;
+
+    public String Surname { get; set; } = surname;
+
+    public String Email { get; set; } = email;
+
+    private List<Vehicle> _vehicles = new List<Vehicle>();
+
+    public void AddVehicle(Vehicle v)
+    {
+        _vehicles.Add(v);
+    }
+
+    public void RemoveVehicle(Vehicle v)
+    {
+        _vehicles.Remove(v);
+    }
+}
---
+++ CarWorkshopManager/Models/Vehicle.cs
@@ -0,0 +1,10 @@
+﻿namespace CarWorkshopManager.Models;
+
+public class Vehicle(string vin, string plateNo)
+{
+    public int Id { get; set; }
+    
+    public string Vin { get; set; } = vin;
+
+    public string PlateNo { get; set; } = plateNo;
+}
